c2eefeeceb1441b078c59148c59235ba4c011ee8
emba2ra3star <tachi.i.061@gmail.com>
2024-03-29 15:46:14 +0800

    測試

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MyCalendar from "./src/component/calendar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MyCalendar />
+      <Text>{MyCalendar.dayOfWeekNames}</Text>
     </View>
   );
 }
---
+++ src/component/calendar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+
+export default function MyCalendar() {
+    return (
+        <View style={{ flex: 1 }}>
+            <Calendar
+                // 在此设置月历的属性
+                // 比如initialMonth, minDate, maxDate等等
+                // dayOfWeekNames={['日', '一', '二', '三', '四', '五', '六']}
+                // dayOfWeekFormat={(dayOfWeek) => {
+                //     return dayOfWeek.substr(0, 1); // 仅显示星期的首字母
+                // }}
+
+                current={'2024-02-22'} // 初始显示的日期
+                minDate={'2024-01-01'} // 允许选择的最早日期
+                maxDate={'2024-12-31'} // 允许选择的最晚日期
+                onDayPress={(day) => { console.log('selected day', day) }} // 选择日期时触发的回调
+                markedDates={{
+                    '2024-03-26': { selected: true, marked: true, selectedColor: '#EE7B7B' }, // 标记特定日期
+                    '2024-03-27': { marked: true },
+                    '2024-03-28': { disabled: true }
+                }}
+            />
+        </View>
+    );
+}
