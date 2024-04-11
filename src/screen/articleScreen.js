4addb3ca6eb48bc6f3e12a7a6f822a98e59b79d4
emba2ra3star <tachi.i.061@gmail.com>
2024-04-11 14:06:37 +0800

    未完成

--- src/screen/articleScreen.js
+++ src/screen/articleScreen.js
@@ -1,5 +1,21 @@
-const ArticleScreen=()=>{
+import { Image, ScrollView, Text, View } from "react-native";
 
+
+const ArticleScreen = () => {
+    <ScrollView>
+        <Text>【首頁】</Text>
+        <View>
+            <Text>1.經期設定：個人化設定經期與週期長度</Text>
+            <View>
+                <Image></Image>
+                <View>
+                    <Text>經期長度：</Text>
+                    <Text>更改經期開始至結束的時間長度，改變預設標記時長。</Text>
+                </View>
+            </View>
+        </View>
+
+    </ScrollView>
 };
 
 export default ArticleScreen;
