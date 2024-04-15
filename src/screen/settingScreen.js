6cbfbd4598e562e2ea4109c349f593d8c4db2f2b
emba2ra3star <tachi.i.061@gmail.com>
2024-04-15 13:08:45 +0800

    aaa

--- src/screen/settingScreen.js
+++ src/screen/settingScreen.js
@@ -4,132 +4,143 @@ import { Switch, Text, View, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import { myStyle } from '../darkMode/style';
+import { useDarkMode } from '../darkMode/DarkModeContext';
 
 const SettingScreen = () => {
-    const { navigate } = useNavigation();
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-    return (
-        <View style={styles.container}>
-            <View style={styles.settingcontent}>
-                <Text style={styles.title}>經期設定</Text>
-                <View style={styles.listContent}>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>經期長度</Text>
-                            <Text>5天</Text>
-                        </Pressable>
-                    </View>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>週期長度</Text>
-                            <Text>28天</Text>
-                        </Pressable>
-                    </View>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>自動推算</Text>
-                            <Switch
-                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
-                                thumbColor={isEnabled ? '#FF5656' : 'white'}
-                                ios_backgroundColor="white"
-                                onValueChange={toggleSwitch}
-                                value={isEnabled}
-                            />
-                        </Pressable>
-                    </View>
-                </View>
-            </View>
+    const { navigate } = useNavigation();
+    const [isAutoCalculateEnabled, setIsAutoCalculateEnabled] = useState(false);
+    const [isPeriodNotificationEnabled, setIsPeriodNotificationEnabled] = useState(false);
+    const [isRecordReminderEnabled, setIsRecordReminderEnabled] = useState(false);
+    const [isBodyStatusReminderEnabled, setIsBodyStatusReminderEnabled] = useState(false);
+   
+    const { isDarkModeEnabled, toggleDarkMode } = useDarkMode();
 
-            <View style={styles.settingcontent}>
-                <Text style={styles.title}>通知設定</Text>
-                <View style={styles.listContent}>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>經期開始/結束</Text>
-                            <Switch
-                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
-                                thumbColor={isEnabled ? '#FF5656' : 'white'}
-                                ios_backgroundColor="white"
-                                onValueChange={toggleSwitch}
-                                value={isEnabled}
-                            />
-                        </Pressable>
-                    </View>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>紀錄提醒</Text>
-                            <Switch
-                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
-                                thumbColor={isEnabled ? '#FF5656' : 'white'}
-                                ios_backgroundColor="white"
-                                onValueChange={toggleSwitch}
-                                value={isEnabled}
-                            />
-                        </Pressable>
-                    </View>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>身體狀態提醒</Text>
-                            <Switch
-                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
-                                thumbColor={isEnabled ? '#FF5656' : 'white'}
-                                ios_backgroundColor="white"
-                                onValueChange={toggleSwitch}
-                                value={isEnabled}
-                            />
-                        </Pressable>
-                    </View>
-                </View>
-            </View>
+    const toggleAutoCalculate = () => setIsAutoCalculateEnabled(previousState => !previousState);
+    const togglePeriodNotification = () => setIsPeriodNotificationEnabled(previousState => !previousState);
+    const toggleRecordReminder = () => setIsRecordReminderEnabled(previousState => !previousState);
+    const toggleBodyStatusReminder = () => setIsBodyStatusReminderEnabled(previousState => !previousState);
+    return (
+        <View style={[myStyle.container, isDarkModeEnabled && myStyle.darkModeContainer]}>
+            <View style={styles.settingcontent}>
+                <Text style={styles.title}>經期設定</Text>
+                <View style={styles.listContent}>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>經期長度</Text>
+                            <Text>5天</Text>
+                        </Pressable>
+                    </View>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>週期長度</Text>
+                            <Text>28天</Text>
+                        </Pressable>
+                    </View>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>自動推算</Text>
+                            <Switch
+                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
+                                thumbColor={isAutoCalculateEnabled ? '#FF5656' : 'white'}
+                                ios_backgroundColor="white"
+                                onValueChange={toggleAutoCalculate}
+                                value={isAutoCalculateEnabled}
+                            />
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
 
-            <View style={styles.settingcontent}>
-                <Text style={styles.title}>偏好設定</Text>
-                <View style={styles.listContent}>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>深色模式</Text>
-                            <Switch
-                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
-                                thumbColor={isEnabled ? '#FF5656' : 'white'}
-                                ios_backgroundColor="white"
-                                onValueChange={toggleSwitch}
-                                value={isEnabled}
-                            />
-                        </Pressable>
-                    </View>
-                    <View>
-                        <Pressable style={styles.listItem}>
-                            <Text>變更背景</Text>
-                            <MaterialCommunityIcons name="camera" color={"black"} size={26} />
-                        </Pressable>
-                    </View>
-                </View>
-            </View>
+            <View style={styles.settingcontent}>
+                <Text style={styles.title}>通知設定</Text>
+                <View style={styles.listContent}>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>經期開始/結束</Text>
+                            <Switch
+                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
+                                thumbColor={isPeriodNotificationEnabled ? '#FF5656' : 'white'}
+                                ios_backgroundColor="white"
+                                onValueChange={togglePeriodNotification}
+                                value={isPeriodNotificationEnabled}
+                            />
+                        </Pressable>
+                    </View>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>紀錄提醒</Text>
+                            <Switch
+                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
+                                thumbColor={isRecordReminderEnabled ? '#FF5656' : 'white'}
+                                ios_backgroundColor="white"
+                                onValueChange={toggleRecordReminder}
+                                value={isRecordReminderEnabled}
+                            />
+                        </Pressable>
+                    </View>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>身體狀態提醒</Text>
+                            <Switch
+                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
+                                thumbColor={isBodyStatusReminderEnabled ? '#FF5656' : 'white'}
+                                ios_backgroundColor="white"
+                                onValueChange={toggleBodyStatusReminder}
+                                value={isBodyStatusReminderEnabled}
+                            />
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
 
-            <View style={styles.settingcontent}>
-                <Text style={styles.title}>其他</Text>
-                <View style={styles.listContent}>
-                    <View>
-                        <Pressable style={styles.listItem} onPress={() => navigate('stack使用教學')}>
-                            <Text>使用教學</Text>
-                            <MaterialCommunityIcons name="chevron-right" color={"black"} size={26} />
-                        </Pressable>
-                    </View>
-                </View>
-            </View>
-        </View>
-    )
+            <View style={styles.settingcontent}>
+                <Text style={styles.title}>偏好設定</Text>
+                <View style={styles.listContent}>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>深色模式</Text>
+                            <Switch
+                                trackColor={{ false: '#F2D7C2', true: '#FF5656' }}
+                                thumbColor={isDarkModeEnabled ? '#FF5656' : 'white'}
+                                ios_backgroundColor="white"
+                                onValueChange={toggleDarkMode}
+                                value={isDarkModeEnabled}
+                            />
+                        </Pressable>
+                    </View>
+                    <View>
+                        <Pressable style={styles.listItem}>
+                            <Text>變更背景</Text>
+                            <MaterialCommunityIcons name="camera" color={"black"} size={26} />
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+
+            <View style={styles.settingcontent}>
+                <Text style={styles.title}>其他</Text>
+                <View style={styles.listContent}>
+                    <View>
+                        <Pressable style={styles.listItem} onPress={() => navigate('stack使用教學')}>
+                            <Text>使用教學</Text>
+                            <MaterialCommunityIcons name="chevron-right" color={"black"} size={26} />
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create(
-    {
-        container:{flex:1,backgroundColor:"white"},
-        settingcontent:{marginHorizontal:30,marginTop:10},
-        title: { color: "#FF9E9E" },
-        listContent: { backgroundColor: "#FFEEE1", borderRadius: 10 ,paddingVertical:5},
-        listItem: { flexDirection: "row", justifyContent: "space-between",alignItems:"center",paddingHorizontal:20,paddingVertical:5 }
-    }
+    {
+        container:{flex:1,backgroundColor:"white"},
+        settingcontent:{marginHorizontal:30,marginTop:10},
+        title: { color: "#FF9E9E" },
+        listContent: { backgroundColor: "#FFEEE1",opacity:0.9, borderRadius: 10 ,paddingVertical:5},
+        listItem: { flexDirection: "row", justifyContent: "space-between",alignItems:"center",paddingHorizontal:20,paddingVertical:5 }
+    }
 );
 
 export default SettingScreen;
