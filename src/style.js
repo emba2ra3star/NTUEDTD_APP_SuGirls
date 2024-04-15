9ef62675ace6a32d7cce03b2fc4374ede96ee8b1
sunnywang930410 <131428028+sunnywang930410@users.noreply.github.com>
2024-04-15 12:50:16 +0800

    Add files via upload

---
+++ src/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const myStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    color: 'black', 
+  },
+  darkModeContainer: {
+    flex: 1,
+    backgroundColor: '#333333',
+    color: 'white', 
+  },
+});
