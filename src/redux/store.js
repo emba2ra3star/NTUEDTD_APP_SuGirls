4526048b6da8c393de30e98913f0de62c54a11ac
sunnywang930410 <131428028+sunnywang930410@users.noreply.github.com>
2024-05-31 17:29:49 +0800

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,17 +2,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import darkModeSlice from './darkModeSlice';
 import notesReducer  from "./notesSlice";
 import selectedDateReducer from "./selectedDateSlice";
-import flowReducer from "./flowSlice";
-import painDegreeReducer from './painDegreeSlice';
+import flowReducer from "./flowSlice"
+import painDegreeReducer from "./painDegreeSlice"
 import syndromesReducer from './syndromesSlice';
+import analyzedataReducer from './analyzedataSlice';
 
 const store = configureStore({
   reducer: {
     darkMode:darkModeSlice,
     selectedDate: selectedDateReducer,
     notes: notesReducer,
-    flow:flowReducer,
+    flow: flowReducer,
     painDegree:painDegreeReducer,
+    analyzedata:analyzedataReducer,
     syndromes:syndromesReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
