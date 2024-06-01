0e2ca916b73728fef73b907871c52aa7c531c99c
emba2ra3star <tachi.i.061@gmail.com>
2024-06-01 17:03:52 +0800

    loading動畫

--- App.js
+++ App.js
@@ -5,17 +5,29 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Navigation from './src/navigation/index';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
+import LottieView from 'lottie-react-native';
+import { useEffect, useState } from 'react';
 
 // import { Provider } from 'react-redux';
 // import { DarkModeProvider } from './src/darkMode/DarkModeContext';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // 模擬一個加載過程
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // 3秒後結束加載
+  }, []);
+  
   return (
     // <DarkModeProvider>
 
     <SafeAreaView style={styles.container}>
       <Provider store={store}>
-        <Navigation />
+        {isLoading?<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <LottieView source={require("./src/json/loadingAnimation.json")} style={{ width: 300, height: 300 }} loop autoPlay />
+        </View>:<Navigation />}
       </Provider>
     </SafeAreaView>
 
